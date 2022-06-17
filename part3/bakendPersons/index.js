4e5ad027aa4ba356b660ecefc7d5603e33b026c2
Edgardo Pinto <edgardo.16jp@gmail.com>
2022-06-17 13:23:24 -0500

    eslint added

--- part3/bakendPersons/index.js
+++ part3/bakendPersons/index.js
@@ -1,6 +1,5 @@
-
-import express, { json, request } from "express"
-import morgan from "morgan"
+import express from 'express'
+import morgan from 'morgan'
 import cors from 'cors'
 import Person from './models/person.js'
 import 'dotenv/config'
@@ -16,65 +15,44 @@ morgan.token('ob', function (req) {
 })
 app.use(morgan(':method :url :status :response-time :req[header] :ob'))
 
-app.get('/api/people',(request, response)=>{
-  Person.find({}).then(person =>{
+app.get('/api/people',(request, response) => {
+  Person.find({}).then(person => {
     response.json(person)
-  })})
-
-app.get('/api/info',(request, response)=>{
-    const entrys= Person.length
-    response.send(`<div>
-    <h3>Notebook has info for ${entrys} people</h3>
-    <h3>${new Date()}</h3>
-    </div>`)
-
-})
-app.get('/api/people/:id',(request, response)=>{
-  Person.findById(request.params.id).then(result =>{
-   response.json(result)
- })
+  })
 })
 
-app.delete('/api/people/:id',(request, response, next)=>{
-       
-    Person.findByIdAndRemove(request.params.id)
-    .then(result => {
+
+app.get('/api/people/:id',(request, response) => {
+  Person.findById(request.params.id).then(result  => {
+    response.json(result) })
+})
+
+app.delete('/api/people/:id',(request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(() => {
       response.status(204).end()
     })
-    .catch(error => next(error))
-   
-  })
+    .catch(error => next(error)) })
 
+app.post('/api/people',(request, response, next) => {
+  const body= request.body
 
-
-
-app.post('/api/people',(request, response, next)=>{
- const body= request.body
-
- if(!body.name || !body.number ){
+  if(!body.name || !body.number ){
     return response.status(404).json({
-        error: 'The name or number is missing '
+      error: 'The name or number is missing '
+    })}
+  Person.find({}).then(persons => {
+    console.log('persons: ', persons)
+    const person = new Person({
+      name: body.name,
+      number: body.number
     })
-}
-Person.find({}).then(persons => {
-  console.log('persons: ', persons)
-  // I commented this because I want to use the put, if the name is already in the server I want to replace the number
-  // if (persons.some(person => person.name === body.name)) {
-  //     console.log("name must be unique")
-  //     return response.status(400).json({
-  //         error: 'name must be unique'
-  //     })
-  // }
-  const person = new Person({
-    name: body.name,
-    number: body.number
-})
 
-    person.save().then(savedPerson =>{
+    person.save().then(savedPerson => {
       response.json(savedPerson)
     })
-    .catch(error => next(error))
-})
+      .catch(error => next(error))
+  })
 })
 
 app.put('/api/people/:id', (request, response, next) => {
--- part3/bakendPersons/models/person.js
+++ part3/bakendPersons/models/person.js
@@ -1,27 +1,27 @@
-import mongoose from "mongoose"
+import mongoose from 'mongoose'
 import 'dotenv/config'
 
 const url = process.env.MONGODB_URI
 mongoose.connect(url)
-  .then(result => {    console.log('connected to MongoDB')  })  
-  .catch((error) => {    console.log('error connecting to MongoDB:', error.message)  })
+  .then(() => { console.log('connected to MongoDB') })
+  .catch((error) => { console.log('error connecting to MongoDB:', error.message) })
 
-  const personSchema = new mongoose.Schema({
-    name:{
-      minlength:3,
-     type: String,
-     required:true
-    } ,
-    number:{
-      minlength:8,
+const personSchema = new mongoose.Schema({
+  name: {
+    minlength: 3,
     type: String,
-    required:true
-   }
-  })
-  
-  personSchema.set('toJSON', {
+    required: true
+  },
+  number: {
+    minlength: 8,
+    type: String,
+    required: true
+  }
+})
+
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
+    returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
   }
