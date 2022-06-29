62bb84d0dbee3d227b975313a99e1e15894ba0bb
Edgardo Pinto <edgardo.16jp@gmail.com>
2022-06-29 14:53:38 -0500

    all test done

--- part4/blogList/test/note_api.test.js
+++ part4/blogList/test/note_api.test.js
@@ -3,9 +3,10 @@ const supertest = require('supertest')
 const helper = require('./test_helper')
 const app = require('../app.js')
 const api = supertest(app)
-const Blog = require('../models/blog.js')
 const bcrypt = require('bcrypt')
 const User = require('../models/user')
+const token = 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImVkam8xNiIsImlkIjoiNjJiOGFjNjM4ZWU0NTYwODg2ZDRjYTk3IiwiaWF0IjoxNjU2Mzc2MDA4fQ.8N68VV7jWPct74PHMPNj4aQfCIZevMllyNRE_QKCUC4'
+const Blog = require('../models/blog')
 
 beforeEach(async () => { 
    await Blog.deleteMany({})  
@@ -16,20 +17,25 @@ beforeEach(async () => {
   })
 
 describe('blogs', ()=>{
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs/')
-    .expect(200)
-    .expect('Content-Type', "application/json; charset=utf-8")
-})
-
+ 
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs/')
+      .expect(200)
+      .set('Authorization', token)
+      .expect('Content-Type', "application/json; charset=utf-8")
+  })
+ 
+   
 test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
+  
+  const response = await api.get('/api/blogs') .set('Authorization', token)
 
   expect(response.body).toHaveLength(helper.initialBlogs.length)})
 
 test('a specific blog is within the returned blogs', async () => {
-  const response = await api.get('/api/blogs')
+  const response = await api.get('/api/blogs').set('Authorization', token)
+
 
   const contents = response.body.map(r => r.title)
     expect(contents).toContain(    'Avatar'  )
@@ -41,6 +47,7 @@ test('a specific blog is within the returned blogs', async () => {
     
       await api
         .post('/api/blogs')
+        .set('Authorization', token)
         .send(newBlog)
         .expect(400)
     const blogsAtEnd = await helper.blogsInDb()
@@ -54,24 +61,26 @@ test('a specific blog can be viewed', async () => {
 
   const blogToView = blogsAtStart[0]      
   const resultBlog = await api    
-  .get(`/api/blogs/${blogToView.id}`)   
+  .get(`/api/blogs/${blogToView.id}`)
+  .set('Authorization', token)
   .expect(200)
-  .expect('Content-Type', /application\/json/)
 
   const processedblogToView = JSON.parse(JSON.stringify(blogToView))
   expect(resultBlog.body).toEqual(processedblogToView)
   
 })
 
-test('a blog can be deleted', async (req) => {
-  const token = req.token
+test('a blog can be deleted', async () => {
+  const form = { username: 'root', password: 'sekret'}
+  const response =await api.post('/api/login').send(form)
+  const token2 =`bearer ${response.body.token}`
   const blogsAtStart = await helper.blogsInDb()
   const blogToDelete = blogsAtStart[0]
 
   await api 
   .delete(`/api/blogs/${blogToDelete.id}`)
-  .set('Authorization', token) 
-    .expect(204)
+  .set('Authorization', token2)
+  .expect(204)
   const blogsAtEnd = await helper.blogsInDb()
 
   expect(blogsAtEnd).toHaveLength(
@@ -85,36 +94,18 @@ test('a blog can be deleted', async (req) => {
 
 
 test('identifitor is called id not _id', async()=>{
-  const response = await api.get('/api/blogs')
+  const response = await api.get('/api/blogs').set('Authorization', token)
+
+  .set('Authorization', token)
   const content = response.body.map(item => item.id)
   expect(content).toBeDefined()
 })
 
-test('se crea nuevo post', async()=>{
-  const newPost ={
-title:"Stranger Things",
-author:" the strangers brothers",
-url:"strange.com",
-likes:10
-  }
-
-  await api.post('/api/blogs').send(newPost)
-  const response = await api.get('/api/blogs')
-
-  expect(response.body).toHaveLength(helper.initialBlogs.length+1)
-})
-
-test('likes is 0 by default', async()=>{
-  const newBlog={
-    "author": "Julius thompson",
-     "title": "Euphoria",
-     "url": "euphoria.com"}
-     
-  const response= await api.post('/api/blogs').send(newBlog)
-  expect(response.body.likes).toBe(0)
-})
-
 test('propertys title and url are integrated', async()=>{
+  const form = { username: 'root', password: 'sekret'}
+  const response =await api.post('/api/login').send(form)
+  const token2 =`bearer ${response.body.token}`
+
   const newPost ={
 title:"Stranger Things",
 author:" the strangers brothers",
@@ -122,15 +113,16 @@ url:"strange.com",
 likes:10
   }
 
-  await api.post('/api/blogs').send(newPost)
-  const response = await api.get('/api/blogs')
-  expect(response.body.title).toBeDefined()
-  expect(response.body.url).toBeDefined()
-})
+  const response2=await api.post('/api/blogs')
+  .set('Authorization', token2)
+  .send(newPost)
+
+  expect(response2.body.title).toBeDefined()
+  expect(response2.body.url).toBeDefined()
 })
 
-
-//... users test
+})
+// //... users test
 
 describe('when there is initially one user in db', () => {
   beforeEach(async () => {
@@ -138,7 +130,6 @@ describe('when there is initially one user in db', () => {
 
     const passwordHash = await bcrypt.hash('sekret', 10)
     const user = new User({ username: 'root', passwordHash })
-
     await user.save()
   })
 
@@ -166,13 +157,7 @@ describe('when there is initially one user in db', () => {
 
   test('creation fails with proper statuscode and message if username already taken', async () => {
     const usersAtStart = await helper.usersInDb()
-    const token =request => {
-      const authorization = request.get('authorization') 
-       if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-         return authorization.substring(7)  
-        }  
-        return null
-      }
+  
     const newUser = {
       username: 'root',
       name: 'Superuser',
@@ -181,17 +166,49 @@ describe('when there is initially one user in db', () => {
 
     const result = await api
       .post('/api/users')
-      .set({ Authorization: token })
       .send(newUser)
       .expect(400)
       .expect('Content-Type', /application\/json/)
 
-    expect(result.body.error).toContain('username must be unique')
+    expect(result.body.error).toContain("User validation failed: username: Error, expected `username` to be unique. Value: `root`")
 
     const usersAtEnd = await helper.usersInDb()
     expect(usersAtEnd).toHaveLength(usersAtStart.length)
   })
+  test('likes is 0 by default', async()=>{
+    const form = { username: 'root', password: 'sekret'}
+ const response =await api.post('/api/login').send(form)
+ const token2 =`bearer ${response.body.token}`
+    const newBlog={
+      author: "Julius thompson",
+       title: "Euphoria",
+       url: "euphoria.com",
+      }
+       
+    const response2= await api.post('/api/blogs').set('Authorization', token2).send(newBlog)
+     expect(response2.body.likes).toBe(0)
+  })
+  test('se crea nuevo post', async()=>{
+    const form = { username: 'root', password: 'sekret'}
+    const response =await api.post('/api/login').send(form)
+    const token2 =`bearer ${response.body.token}`
+    
+    const newPost ={
+  title:"Stranger Things",
+  author:" the strangers brothers",
+  url:"strange.com",
+  likes:10
+    }
+  
+    await api.post('/api/blogs')  
+    .set('Authorization', token2)
+    .send(newPost)
+    
+    const response2 = await api.get('/api/blogs').set('Authorization', token2)
+    expect(response2.body).toHaveLength(helper.initialBlogs.length+1)
+  })
 })
+
 
 
 
